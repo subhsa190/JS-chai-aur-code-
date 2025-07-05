f02ee74d8ed4f78c732d1e70a7d686e2d8f730f6
subham saha <subham42cse@gmail.com>
2025-07-05 07:15:57 +0000

    comparison discussion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);  // equality check == and comparisons > < >= <= work differently 
+console.log(null >= 0);  // comparisons convert null to a number,treating it as 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
