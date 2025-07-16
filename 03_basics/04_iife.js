711a24d9f15a9d1d644f6a0bd5abbb0e95f98950
subham saha <subham42cse@gmail.com>
2025-07-16 07:48:38 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,8 +1,14 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 // global scope ke polution(variable) ko hatane ke liye IIFE use hota hain
+
 (function chai(){
+    // named IIFE
     console.log(`DB CONNECTED`);  
-})()
+})();
 
 // ()() ---> first () is function definition , second () is function call
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+} )('subham')
