97d843c2a85aa4c518b9f01591ab9d83a29e16cf
subham saha <subham42cse@gmail.com>
2025-07-17 06:41:04 +0000

    if-else

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "january":
+        console.log("january");       
+        break;
+    case "february":
+        console.log("february");       
+        break;
+    case "march":
+        console.log("march");       
+        break;
+    case "april":
+        console.log("april");       
+        break;
+    default:
+        console.log("default case match");       
+        break;
+}
